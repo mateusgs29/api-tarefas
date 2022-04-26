1290d88edebc2c31f46ca02cdcfcf50f8d7384d3
Mateus Gomes <mateus.gs1999@gmail.com>
2022-04-26 11:50:32 -0300

    rota para documentacao do projeto

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express")
 const bodyParser = require('body-parser')
+
 const roteadorTasks = require("./src/routes/tasks.routes")
+
 const NotFound = require("./src/errors/NotFound")
 const InvalidField = require("./src/errors/InvalidField")
 const DataNotProvided = require("./src/errors/DataNotProvided")
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerDoc = require('./swagger.json')
 
 const app = express()
 const port = 3000
 
 app.use(bodyParser.json())
 
-app.use('/api/tarefas', roteadorTasks)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+
+app.use('/tarefas', roteadorTasks)
 
 app.use((err,  req, res, next) => {
   let status = 500
